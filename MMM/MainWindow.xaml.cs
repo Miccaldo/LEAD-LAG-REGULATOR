aad9160c674964fd65c76776d75d7b17239a72be
Michał Janowski <miccaldo@gmail.com>
2019-02-16 22:20:47 +0100

    dodano nowy projekt

---
+++ MMM/MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using OxyPlot;
+using MMM;
+
+namespace MMM {
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+     
+        public Regulator regulator { get; set; }
+        public int amplitude { get; private set; }
+        public int periodTriangle { get; set; }
+        public int stepTime { get; set; }
+        public int typePlotFlag { get; set; }
+
+        public MainWindow() {
+
+            InitializeComponent();
+
+            amplitude = 1;
+            typePlotFlag = 1;
+            periodTriangle = amplitude;
+
+            regulator = new Regulator();
+
+            stepTime = regulator.timeRange;
+
+            RadioButton_Step.IsChecked = true;
+
+            parametersGrid.DataContext = regulator;
+
+            //MessageBox.Show($"{regulator.w11}, {regulator.w12}, {regulator.w13}, {regulator.w14}");
+            //MessageBox.Show($"{regulator.w21}, {regulator.w22}, {regulator.w23}, {regulator.w24}");
+        }
+
+       // ************************ Zdarzenia oblugujace parametry regulatora  ************************
+
+        private void a1_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if(Double.TryParse(a1_Box.Text, out value) == true) {
+                    regulator.a1 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    a1_Box.Text = null;
+                }
+            }
+        }
+        private void a0_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(a0_Box.Text, out value) == true) {
+                    regulator.a0 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    a0_Box.Text = null;
+                }
+            }
+        }
+        private void b2_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(b2_Box.Text, out value) == true) {
+                    regulator.b2 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    b2_Box.Text = null;
+                }
+            }
+        }
+        private void b1_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(b1_Box.Text, out value) == true) {
+                    regulator.b1 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    b1_Box.Text = null;
+                }
+            }
+        }
+        private void b0_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(b0_Box.Text, out value) == true) {
+                    regulator.b0 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    b0_Box.Text = null;
+                }
+            }
+        }
+        private void z1_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(z1_Box.Text, out value) == true) {
+                    regulator.z1 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    z1_Box.Text = null;
+                }
+            }
+        }
+        private void z0_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(z0_Box.Text, out value) == true) {
+                    regulator.z0 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    z0_Box.Text = null;
+                }
+            }
+        }
+        private void p1_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(p1_Box.Text, out value) == true) {
+                    regulator.p1 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    p1_Box.Text = null;
+                }
+            }
+        }
+        private void p0_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                double value;
+                if (Double.TryParse(p0_Box.Text, out value) == true) {
+                    regulator.p0 = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane");
+                    p0_Box.Text = null;
+                }
+            }
+        }
+
+        // ************************ Zdarzenia oblugujace czas trwania sygnalu, amplitude, czas trwania skoku jednostkowego  ************************
+
+
+        private void Time_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                int value;
+                if (int.TryParse(Time_Box.Text, out value) == true) {
+                    regulator.timeRange = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane(int)");
+                    Time_Box.Text = null;
+                }
+            }
+        }
+
+        private void Amplitude_KeyDownHandler(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                int value;
+                if (int.TryParse(Amplitude_Box.Text, out value) == true) {
+                    this.amplitude = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane(int)");
+                    Amplitude_Box.Text = null;
+                }
+            }
+        }
+
+        private void StepTime_KeyDownHandler(object sender, KeyEventArgs e) {  
+            if (e.Key == Key.Return) {
+                int value;
+                if (int.TryParse(StepTime_Box.Text, out value) == true) {
+                    this.stepTime = value;
+                    UpdateGrid();
+                } else {
+                    MessageBox.Show("Nieprawidłowe dane(int)");
+                    StepTime_Box.Text = null;
+                }
+            }
+        }
+
+
+        // ************************ -----------------------------  ************************
+
+
+        private void UpdateGrid() {             //  Metoda aktualizuje wykres
+
+            regulator = new Regulator(regulator.a0, regulator.a1, regulator.b0, regulator.b1, regulator.b2, regulator.z0, regulator.z1, regulator.p0, regulator.p1, regulator.timeRange);
+            SelectStimulation();
+
+            gridek.DataContext = regulator;
+        }
+
+
+        private double Step(double time) {      // Funkcje skoku, sinusa, trojkata
+            if(time <= stepTime)
+                return amplitude * 1;
+            else {
+                return 0;
+            }
+        }
+
+        private double Sinus(double time) {         
+            return amplitude * Math.Sin(time);
+        }
+
+        private double Triangle(double time) {         
+            return (amplitude / periodTriangle) * (periodTriangle - Math.Abs(time % (2 * periodTriangle) - periodTriangle));
+        }
+
+        private void RadioButton_Step_Checked(object sender, RoutedEventArgs e) {       // Zdarzenia wyboru pobudzenia 
+            UpdateGrid();
+            RadioButton_Sinus.Margin = new Thickness(0, 50, 0, 0);
+            RadioButton_Triangle.Margin = new Thickness(0, 80, 0, 0);
+            StepTime_Box.Visibility = Visibility.Visible;
+        }
+
+        private void RadioButton_Sinus_Checked(object sender, RoutedEventArgs e) {
+            UpdateGrid();
+            RadioButton_Sinus.Margin = new Thickness(0, 30, 0, 0);
+            RadioButton_Triangle.Margin = new Thickness(0, 60, 0, 0);
+            StepTime_Box.Visibility = Visibility.Hidden;
+        }
+
+        private void RadioButton_Triangle_Checked(object sender, RoutedEventArgs e) {
+            UpdateGrid();
+            RadioButton_Sinus.Margin = new Thickness(0, 30, 0, 0);
+            RadioButton_Triangle.Margin = new Thickness(0, 60, 0, 0);
+            StepTime_Box.Visibility = Visibility.Hidden;
+        }
+
+        private void SelectStimulation() {      // Metoda rysuje wykres w zaleznosci od pobudzenia
+
+            if(RadioButton_Step.IsChecked == true){
+                regulator.DrawPlot(Step, typePlotFlag);
+            }else if(RadioButton_Sinus.IsChecked == true) {
+                regulator.DrawPlot(Sinus, typePlotFlag);
+            } else {
+                regulator.DrawPlot(Triangle, typePlotFlag);
+            }
+        }
+
+        private void TypePlot_Click(object sender, RoutedEventArgs e) {     // Zdarzenie zmieniajace rodzaj wykresu (wejscie lub wyjscie)
+
+            typePlotFlag++;
+
+            if (typePlotFlag > 1) {
+                y_axis.Content = "u(t)";
+                typePlotButton.Content = "Odpowiedź";
+                typePlotFlag = 0;
+            } else {
+                y_axis.Content = "y(t)";
+                typePlotButton.Content = "Pobudzenie";
+            }
+
+            UpdateGrid();
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e) {
+
+            regulator = new Regulator();
+            UpdateGrid();
+            parametersGrid.DataContext = regulator;
+        }
+    }
+}
---
+++ MMM/Regulator.cs
@@ -0,0 +1,89 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMM {
+
+    public class Regulator {
+
+        public delegate double Stimulation(double x);
+
+        public List<DataPoint> Points { get; private set; }
+
+        private double dt = 0.001;
+        public int timeRange { get; set; }
+
+        public double a1 { get; set; }              // Parametry regulatora
+        public double a0 { get; set; }
+        public double b2 { get; set; }
+        public double b1 { get; set; }
+        public double b0 { get; set; }
+        public double p1 { get; set; }
+        public double p0 { get; set; }
+        public double z1 { get; set; }
+        public double z0 { get; set; }
+
+        private double x1, x2, x3, x4, dx1, dx2, dx3, dx4;
+        public double w11, w12, w13, w14, w21, w22, w23, w24;      // Zmienne przechowujace wartosci macierzy stanu
+
+
+        public void DrawPlot(Stimulation stimulation, int type) {       // Funkcja oblicza zmienne stanu metoda Eulera oraz tworzy wykres sygnalu wyjsciowego lub wejsciowego
+
+            Points = new List<DataPoint>();
+            Points.Add(new DataPoint(0, 0));
+
+            for (int i = 0; i < timeRange / dt; i++) {
+
+                if (type == 1) {
+                    x1 = x1 + (w11 * dx1 + dx2) * dt + w21 * stimulation(i * dt) * dt;
+                    x2 = x2 + (w12 * dx1 + dx3) * dt + w22 * stimulation(i * dt) * dt;
+                    x3 = x3 + (w13 * dx1 + dx4) * dt + w23 * stimulation(i * dt) * dt;
+                    x4 = x4 + (w14 * dx1) * dt + w24 * stimulation(i * dt) * dt;
+
+                    dx1 = x1;
+                    dx2 = x2;
+                    dx3 = x3;
+                    dx4 = x4;
+
+                    Points.Add(new DataPoint(i * dt, x1));
+                } else {
+                    x1 = stimulation(i * dt);
+                    Points.Add(new DataPoint(i * dt, x1));
+                }
+            }
+        }
+
+        // OBLICZENIA 
+
+        public Regulator(double a0 = 1, double a1 = -1, double b0 = 1, double b1 = 1, double b2 = 1, double z0 = 1, double z1 = 4, double p0 = 2, double p1 = 3, int timeRange = 20) {
+
+            this.a0 = a0;
+            this.a1 = a1;
+            this.b0 = b0;
+            this.b1 = b1;
+            this.b2 = b2;
+            this.z0 = z0;
+            this.z1 = z1;
+            this.p0 = p0;
+            this.p1 = p1;
+
+            w11 = -(a1 / b2 + p0 + p1 + b1 / b2);
+            w12 = -(a1 * z1 / b2 + a0 / b2 + a1 * z0 / b2 + p0 * p1 + b1 * p1 / b2 + b1 * p0 / b2 + b0 / b2);
+            w13 = -(b0 * p0 / b2 + b0 * p1 / b2 + b1 * p0 * p1 / b2 + a0 * z0 / b2 + a0 * z1 / b2 + a1 * z0 * z1 / b2);
+            w14 = -(a0 * z0 * z1 / b2 + b0 * p0 * p1 / b2);
+
+
+        
+
+            w21 = a1 / b2;
+            w22 = (a1 * z1 / b2 + a1 * z0 / b2 + a0 / b2);
+            w23 = (a1 * z0 * z1 / b2 + a0 * z1 / b2 + a0 * z0 / b2);
+            w24 = a0 * z0 * z1 / b2;
+
+            this.timeRange = timeRange;
+        }
+    }
+}
